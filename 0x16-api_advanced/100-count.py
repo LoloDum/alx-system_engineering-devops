d86d0de2d888d973be1dc290ff911329f43bc3ef
LoloDum <chinedumokamgba@gmail.com>
2023-02-28 16:42:28 -0800

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,10 +5,9 @@ import requests
 
 def count_words(subreddit, word_list, found_list=[], after=None):
     '''Prints counts of given words found in hot posts of a given subreddit.
+
     Args:
         subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        found_list (obj): Key/value pairs of words/counts.
         after (str): The parameter for the next page of the API results.
     '''
     user_agent = {'User-agent': 'test45'}
